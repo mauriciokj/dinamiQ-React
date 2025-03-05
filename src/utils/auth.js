c3fd2b70d3202e77c1083d6e4ea029ebd90632fd
Mauricio Krzesinski Junior <mauriciokj@gmail.com>
2025-03-05 18:12:29 -0300

    testes de geracao de uuid

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,9 +1,13 @@
 // src/utils/auth.js
+import { v4 as uuidv4 } from 'uuid'
+
 export const getOrCreateUserId = () => {
-  let userId = localStorage.getItem('pollUserId');
+  let userId = localStorage.getItem('pollUserId')
+  
   if (!userId) {
-    userId = crypto.randomUUID();
-    localStorage.setItem('pollUserId', userId);
+    userId = uuidv4() // Usa a biblioteca UUID
+    localStorage.setItem('pollUserId', userId)
   }
-  return userId;
-};
+  
+  return userId
+}
